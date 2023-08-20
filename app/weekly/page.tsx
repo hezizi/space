e5c52437dc096d621652e80045b0980a50ee9bb4
yucihent <yucihent@163.com>
2023-08-20 21:38:25 +0800

    feat(weekly): :sparkles: 新增0814-20周刊

--- app/weekly/page.tsx
+++ app/weekly/page.tsx
@@ -5,24 +5,23 @@ export default async function Weekly() {
   const weekly = await fetchWeeklyList().catch(() => [])
 
   return (
-    <h2>整理中...🔥</h2>
-    // <ul>
-    //   {weekly.map(({ slug, title }, index: number) => (
-    //     <li
-    //       key={slug}
-    //       className="prose prose-custom dark:prose-invert max-w-none mb-1"
-    //     >
-    //       <span className="mr-3">
-    //         #{index + 1} <time>{slug}</time>
-    //       </span>
-    //       <Link
-    //         href={`/weekly/${slug}`}
-    //         className="hover:text-sky-500 hover:underline dark:text-slate-300 dark:hover:text-sky-500"
-    //       >
-    //         <span>{title}</span>
-    //       </Link>
-    //     </li>
-    //   ))}
-    // </ul>
+    <ul>
+      {weekly.map(({ slug, title }, index: number) => (
+        <li
+          key={slug}
+          className="prose prose-custom dark:prose-invert max-w-none mb-1"
+        >
+          <span className="mr-3 text-gray-600 dark:text-slate-400">
+            #{index + 1} <time>{slug}</time>
+          </span>
+          <Link
+            href={`/weekly/${slug}`}
+            className="hover:text-sky-500 hover:underline dark:text-slate-300 dark:hover:text-sky-500"
+          >
+            <span className="font-bold">{title}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Icon from './Icon'
 
 export default function Footer() {
   return (
-    <footer className="text-sm mt-20 flex items-center text-gray-500">
+    <footer className="text-sm mt-20 flex items-center text-gray-500 dark:text-slate-400">
       <div className="flex">
         <p className="mr-3">Yucihent ©2023-present</p>
         <p>Created by yucihent</p>
