70344abe4fff54200fcb8ec33aa357c17767d18c
yucihent <yucihent@163.com>
2024-01-05 14:15:46 +0800

    feat: :sparkles: 头部链接添加高亮

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,36 @@
+'use client'
+
 import Link from 'next/link'
 import ThemeMode from '@/components/ThemeMode'
 import { LINKS, SPACE_NAME } from '@/constants'
+import { useState } from 'react'
 
 export default function Header() {
+  const [active, setActive] = useState('Home')
+  const onClik = (key: string) => {
+    setActive(key)
+  }
   return (
     <header className="flex justify-between items-center mb-20">
       <Link className="text-xl" href="/">
         {SPACE_NAME}
       </Link>
       <nav className="flex justify-around rounded-full shadow-2xl shadow-gray-400 px-3">
-        {Object.entries(LINKS).map((item) => (
-          <Link
-            key={item[0]}
-            href={item[1]}
-            className="py-3 px-5 transition-transform duration-200 hover:scale-125"
-          >
-            {item[0]}
-          </Link>
-        ))}
+        {Object.entries(LINKS).map((item) => {
+          const className = `py-3 px-5 transition-transform duration-200 hover:text-cyan-500 ${
+            item[0] === active ? 'text-cyan-500' : ''
+          }`
+          return (
+            <Link
+              onClick={() => onClik(item[0])}
+              key={item[0]}
+              href={item[1]}
+              className={className}
+            >
+              {item[0]}
+            </Link>
+          )
+        })}
       </nav>
       <ThemeMode />
     </header>
