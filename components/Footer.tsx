1091ddbbe0c6f99cadec44ae0f477adc374dce38
yucihent <yucihent@163.com>
2024-01-18 21:22:10 +0800

    chore: :hammer: update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,20 @@
-import Icon from './Icon'
-
 export default function Footer() {
   return (
     <footer className="text-sm mt-20 flex items-center text-gray-500 dark:text-slate-400">
       <div className="flex">
-        <p className="mr-3">Yucihent ©2023-present</p>
-        <p>Created by yucihent</p>
+        <a
+          className="underline"
+          target="_blank"
+          href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+          rel="noopener noreferrer"
+        >
+          CC BY-NC-SA 4.0
+        </a>
+        <p className="mx-2">Yucihent ©2023-present</p>
+        <p>
+          Created by <span className="font-bold">yucihent</span>
+        </p>
       </div>
-      <a
-        className="ml-3"
-        href="https://github.com/hezizi/space"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon name="github" width={20} height={20} />
-      </a>
     </footer>
   )
 }
