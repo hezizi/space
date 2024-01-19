2a68020bb2a62b53333bb9d380dacf7cb3a848e9
yucihent <yucihent@163.com>
2024-01-19 21:30:33 +0800

    chore: :hammer: update welcome

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <>
       <p>
-        Hi there👋 欢迎来到我的空间👀, 在下方👇输入命令交互🔥,
-        更多有意思命令即将开放🥳
+        Hi there👋
+        欢迎来到我的空间👀，在下方👇输入命令交互🔥，更多有意思命令即将开放🥳
       </p>
       <CommandProvider versions={versions}>
         <CommandLine />
